3179facb6e0f1e2a69f33ea8ca1929d25840ce1f
elevarwellness <elevarwellness@gmail.com>
2026-03-25 08:16:45 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // Google Sheet export URL — change the spreadsheet ID to point to a different sheet
-  SHEET_URL: "https://docs.google.com/spreadsheets/d/1d7r0PNbs0WiuDo-1E_Tq4RHRldrYussa9hkzaA6rB44/export?format=csv&gid=0",
+  SHEET_URL: "https://docs.google.com/spreadsheets/d/1ooBvvDkJiIheY5IlUbFXACBKmzuvkcpOjPSghxzPz5s/edit?pli=1&gid=0#gid=0",
 
   // Amount added on top of the "Sale Price" column to get the displayed price
-  MARKUP: 350,
+  MARKUP: 0,
 };
